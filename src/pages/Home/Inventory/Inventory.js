88b83f2df2124bd396cb8400ceeb5e74d51155e4
zubayer-salehin <freelancerzubear@gmail.com>
2022-07-01 16:58:39 +0600

    api error solve

--- src/pages/Home/Inventory/Inventory.js
+++ src/pages/Home/Inventory/Inventory.js
@@ -4,24 +4,15 @@ import { useNavigate } from 'react-router-dom';
 import Loading from '../../Shared/Loading/Loading';
 import Product from '../Product/Product';
 import "./Inventory.css";
+import { useQuery } from 'react-query';
 
 const Inventory = () => {
-    const [fruites, setFruites] = useState([]);
+    // const [fruites, setFruites] = useState([]);
     const navigate = useNavigate();
     const [pageCount, setPageCount] = useState(0);
-    const [loading, setLoading] = useState(true);
+    // const [loading, setLoading] = useState(true);
     const [page, setPage] = useState(0);
     const size = 6;
-
-    useEffect(() => {
-        setLoading(true);
-        fetch(`https://immense-tundra-86422.herokuapp.com/fruites?page=${page}&size=${size}`)
-            .then(res => res.json())
-            .then(data => {
-                setFruites(data)
-                setLoading(false);
-            })
-    }, [page, size])
 
     useEffect(() => {
         fetch("https://immense-tundra-86422.herokuapp.com/fruiteCount")
@@ -32,25 +23,47 @@ const Inventory = () => {
             })
     }, [])
 
+    // useEffect(() => {
+    //     setLoading(true);
+    //     fetch(`https://immense-tundra-86422.herokuapp.com/fruites?page=${page}&size=${size}`)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setFruites(data)
+    //             setLoading(false);
+    //         })
+    // }, [page, size])
+
+    const { data: fruites, isLoading, refetch } = useQuery([page, size], () => fetch(`https://immense-tundra-86422.herokuapp.com/fruites?page=${page}&size=${size}`)
+        .then(res => res.json())
+    );
+
+    if (isLoading) {
+        return <Loading loadingStatus="true"></Loading>
+    }
+
+
+
     const handlePreviou = () => {
         if (page > 0) {
             setPage(page - 1);
+            refetch();
         }
     }
 
     const handleNext = () => {
-        if (page < (pageCount-1)) {
+        if (page < (pageCount - 1)) {
             setPage(page + 1);
+            refetch();
         }
     }
 
 
-    return ( loading ? <Loading loadingStatus="true"></Loading> :
+    return (
         <div id='Inventory'>
             <h1 className='my-5 pt-3 pb-1 text-center'>Our Inventory</h1>
             <Container className='mb-4'>
                 <div className="inventory-container mb-5">
-                    {fruites.map(product => <Product key={Math.random() * 10000} product={product}></Product>)}
+                    {fruites?.map(product => <Product key={Math.random() * 10000} product={product}></Product>)}
                 </div>
                 <div className='d-block pt-3 text-center'>
                     <button id='prevNextBtn' onClick={handlePreviou} className='prevBtn'>Prev</button>
