6593d49d92d5c4b923cb9f706234bbea126f15ed
zubayer-salehin <freelancerzubear@gmail.com>
2022-07-17 12:25:18 +0600

    inventory api fetch problame solve

--- src/pages/Home/Inventory/Inventory.js
+++ src/pages/Home/Inventory/Inventory.js
@@ -4,13 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import Loading from '../../Shared/Loading/Loading';
 import Product from '../Product/Product';
 import "./Inventory.css";
-import { useQuery } from 'react-query';
 
 const Inventory = () => {
     const navigate = useNavigate();
     const [pageCount, setPageCount] = useState(0);
+    const [fruites, setFruites] = useState([]);
     const [page, setPage] = useState(0);
     const size = 6;
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         fetch("https://immense-tundra-86422.herokuapp.com/fruiteCount")
@@ -21,32 +22,30 @@ const Inventory = () => {
             })
     }, [])
 
-    const { data: fruites, isLoading, refetch } = useQuery([page, size], () => fetch(`https://immense-tundra-86422.herokuapp.com/fruites?page=${page}&size=${size}`)
-        .then(res => res.json())
-    );
-
-    if (isLoading) {
-        return <Loading loadingStatus="true"></Loading>
-    }
-
-
+    useEffect(() => {
+        setLoading(true);
+        fetch(`https://immense-tundra-86422.herokuapp.com/fruites?page=${page}&size=${size}`)
+            .then(res => res.json())
+            .then(data => {
+                setFruites(data)
+                setLoading(false);
+            })
+    }, [page, size])
 
     const handlePreviou = () => {
         if (page > 0) {
             setPage(page - 1);
-            refetch();
         }
     }
 
     const handleNext = () => {
         if (page < (pageCount - 1)) {
             setPage(page + 1);
-            refetch();
         }
     }
 
 
-    return (
+    return loading ? <Loading loadingStatus="true"></Loading> :
         <div id='Inventory'>
             <h1 className='my-5 pt-3 pb-1 text-center'>Our Inventory</h1>
             <Container className='mb-4'>
@@ -56,7 +55,7 @@ const Inventory = () => {
                 <div className='d-block pt-3 text-center'>
                     <button id='prevNextBtn' onClick={handlePreviou} className='prevBtn'>Prev</button>
                     <div className='pagination d-inline'>
-                        {[...Array(pageCount).keys()].map((number,index) => <button
+                        {[...Array(pageCount).keys()].map((number, index) => <button
                             onClick={() => setPage(number)} className={page === number ? "selected" : ""}
                             key={index}>{number + 1}</button>)}
                     </div>
@@ -67,7 +66,6 @@ const Inventory = () => {
                 <button onClick={() => navigate("/manageItem")} type='button' className='invetoryBtn py-3 px-5 fs-5'>Manage Inventories</button>
             </div>
         </div>
-    );
 };
 
 export default Inventory;
